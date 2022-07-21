60b2212da2b8a0924cd0338b47892dc16c2100fc
Vuk <koprivica.wolf@gmail.com>
2022-07-20 21:44:07 -0700

    Ready for PROD

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -18,6 +18,7 @@ namespace API.Data
     {
         public DataContext(DbContextOptions options) : base(options)
         {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         }
 
         // public DbSet<AppUser> Users { get; set; }
